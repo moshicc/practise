c42f9edfbe0778396f8a23968c2d602e4754697a
张聪聪 <1316435855@qq.com>
2021-06-24 12:00:58 +0800

    1.测试

--- src/main/java/com/zcc/algorithm_practise/other22.java
+++ src/main/java/com/zcc/algorithm_practise/other22.java
@@ -1,8 +1,6 @@
 package com.zcc.algorithm_practise;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * @author zcc
@@ -15,6 +13,11 @@ import java.util.UUID;
 public class other22 {
     public static void main(String[] args) {
 
+        //streamHap();
+        sortBull();
+    }
+
+    private static void streamHap() {
         Map<String, Object> map = new HashMap<>();
 
         for (int i = 0; i < 10; i++) {
@@ -23,6 +26,33 @@ public class other22 {
 
         map.forEach((k, v) -> {
             System.out.println("key:" + k + " ,value:" + v);
+        });
+    }
+
+    public static void sortBull(){
+        int[] arr = new int[100];
+        for (int index = 0; index < 100; index++) {
+            arr[index] = (int) (Math.random()*1000);
+        }
+        Arrays.stream(arr).forEach((v)->{
+            System.out.print(v + ",");
+        });
+        System.out.println("");
+
+
+        for (int i = 0; i < arr.length -1; i++) {
+            for (int j = 0; j <arr.length - i - 1; j++) {
+                //指在什么情况下进行交换，是前大后小（前后交换），还是前小后大（前后交换）
+                if (arr[j] < arr[j+1]) {
+                    //核心都是 前一个 与后一个交换（把后一个先暂存，把前一个赋值个后一个，再把暂存的后一个赋值个前一个）
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        Arrays.stream(arr).forEach(( a )->{
+            System.out.print(a + ",");
         });
     }
 }
