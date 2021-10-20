6eadbed4f25bc9324f971a5e37055192212abd97
张聪聪 <1316435855@qq.com>
2021-10-20 10:13:21 +0800

    1.使用字节流，把目标文件读取出来（缓冲、不缓冲） 2.使用字节流复制文件，测试使用缓冲对时间的影响 3.用inputStreamRead 用指定编码，读取文件内数据

---
+++ src/main/java/com/zcc/io_stream_practise/io/InputStreamTest01.java
@@ -0,0 +1,66 @@
+package com.zcc.io_stream_practise.io;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * @author zcc
+ * @ClassName InputStreamTest01
+ * @description 把数据从 目标文件中读取出来
+ * @date 2021/10/18 9:58
+ * @Version 1.0
+ */
+
+public class InputStreamTest01 {
+    public static void main(String[] args) {
+        fileInput2();
+
+    }
+
+    public static void fileInput(){
+        String srcPath = "e:/f/apple.txt";
+        FileInputStream fis = null;
+        int contxt;
+        try {
+            //构建一个inputStream对象
+            fis = new FileInputStream(srcPath);
+            //每次读取一个字节(单个字节读取)
+            while ((contxt = fis.read()) != -1) {
+                //System.out.println(contxt);
+                System.out.print((char) contxt);
+            }
+        }catch (Exception e) {
+            
+        } finally {
+            try {
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void fileInput2(){
+        String srcPath = "e:/f/apple.txt";
+        FileInputStream fis = null;
+        int index;
+        try {
+            //构建一个inputStream对象
+            fis = new FileInputStream(srcPath);
+            //缓冲区
+            byte[] buf = new byte[8];
+            //每次读取8个字节
+            while ((index = fis.read(buf)) != -1) {
+                System.out.print(new String(buf,0,index));
+            }
+        }catch (Exception e) {
+
+        } finally {
+            try {
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
